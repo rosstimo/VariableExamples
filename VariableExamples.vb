d087c156cd79ca09567be7150fb448f3f0e779ec
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-19 08:53:03 -0700

    add int and string examples

--- VariableExamples.vb
+++ VariableExamples.vb
@@ -1,7 +1,36 @@
-﻿Module VariableExamples
+﻿'header stuff
+
+Option Strict On 'No conversions by compiler
+Option Explicit On 'Must declare all variables
+
+Module VariableExamples 'PascalCase
 
     Sub Main()
+        Dim x As Integer
+        Dim y As Double
+        Dim result As Integer
 
+        Dim personName As String 'camelCase
+        Dim firstName As String
+        Dim lastName As String
+        Dim age As Integer
+
+        x = 1
+        y = 2.5
+        result = x + CInt(y) 'force myself to convert to integer
+
+
+        age% = 42
+        firstName$ = "Joe"
+        lastName$ = "Barnes"
+
+        personName = firstName & " " & lastName
+        Console.WriteLine(personName _
+                            & " is " _
+                            & CStr(age) _
+                            & " years old.") 'wrap long lines with SPACE underscore " _"
+
+        Console.Read()
     End Sub
 
 End Module
