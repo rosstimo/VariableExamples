a7a0f42f11b9c12c89efd8c065049f908a81e167
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-20 08:25:25 -0700

    add constant example

--- VariableExamples.vb
+++ VariableExamples.vb
@@ -8,27 +8,34 @@ Module VariableExamples 'PascalCase
     Sub Main()
         Dim x As Integer
         Dim y As Double
-        Dim result As Integer
+        Dim result As Decimal
+
+        Const TAXRATE = 0.06@ 'type specifier forces desired type
 
         Dim personName As String 'camelCase
         Dim firstName As String
         Dim lastName As String
         Dim age As Integer
 
-        x = 1
-        y = 2.5
-        result = x + CInt(y) 'force myself to convert to integer
+        x = 10
+
+        result = x * TAXRATE
+        'result = result + x
+        result += x
+
+        'y = 2.5
+        'result = x + CInt(y) 'force myself to convert to integer
 
 
-        age% = 42
-        firstName$ = "Joe"
-        lastName$ = "Barnes"
+        'age% = 42
+        'firstName$ = "Joe"
+        'lastName$ = "Barnes"
 
-        personName = firstName & " " & lastName
-        Console.WriteLine(personName _
-                            & " is " _
-                            & CStr(age) _
-                            & " years old.") 'wrap long lines with SPACE underscore " _"
+        'personName = firstName & " " & lastName
+        'Console.WriteLine(personName _
+        '                    & " is " _
+        '                    & CStr(age) _
+        '                    & " years old.") 'wrap long lines with SPACE underscore " _"
 
         Console.Read()
     End Sub
